a3f6abafe06443a4232f90ec5765b968e03db550
Alva00 <1406180578@qq.com>
2018-02-06 11:40:07 +0800

    update

--- longest_palindromic_substring.cpp
+++ longest_palindromic_substring.cpp
@@ -5,17 +5,51 @@ using namespace std;
 /*
  *Given a string s, find the longest palindromic substring in s.
  */
-string longestPalindrome(string s)
+string longestPalindrome_Normal(string s)
 {
+	/* 动态规划 */
+	int n = s.size(); 
+	bool dp[n][n] = {0};
+	int max_len = 1; 
+	int start = 0;
+	
+	for(int i = 0; i < s.size(); i++)  
+	{  
+		for(int j = 0; j <= i; j++)  
+		{
+			if(i == j)
+			{
+				dp[j][i] = true;
+			}
+			else if(i - j == 1)
+			{
+				dp[j][i] = (s[i] == s[j]);  
+			}
+			else if(i - j > 1)
+			{
+				dp[j][i] = (s[i] == s[j] && dp[j + 1][i - 1]);
+			}
+			
+			if(dp[j][i] && max_len < (i - j + 1))  
+			{  
+				max_len = i - j + 1;  
+				start = j;  
+			}   
+		}  
+	}  
+	return s.substr(start, max_len);  
+}
+
+string longestPalindrome_DP(string s)
+{
+	/* 暴力穷举  */
 	string longestString = "";
-	/* split string into substring */
 	for(int i = 0; i < s.length(); i++)
 	{
 		string substring = "";
 		for(int j = i; j < s.length(); j++)
 		{
 			substring += s[j];
-			/* whether substring is palindrome */
 			bool flag = false;
 			for(int k = 0; k < substring.length(); k++)
 			{
@@ -27,19 +61,20 @@ string longestPalindrome(string s)
 			}
 			if(!flag)
 			{
-				if(substring.length() > substring.length())
+				if(substring.length() > longestString.length())
 				{
 					longestString = substring;
 				}
 			}
 		}
 	}
-	return longestString;
-}
+	return longestString;	
+} 
+
 int main()
 {
 	string s;
 	cin >> s;
-	cout << longestPalindrome(s) << endl;
+	cout << longestPalindrome_Normal(s) << endl;
 	return 0;
 }
